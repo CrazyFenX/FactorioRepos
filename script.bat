03ba18e69d5704ae66f792c12d4b8d458c5409a8
CrazyFenX <kattoshkin@gmail.com>
2022-12-18 13:29:37 +0700

    commit by :13:29:37,36

---
+++ script.bat
@@ -0,0 +1,40 @@
+@echo off
+@echo Loggin is: "%1" Password is: "%2"
+
+@echo Git part is starting
+@echo Connecting to GitHub...
+
+rem git config --global user.name "%1"
+rem git config --global user.email "%2"
+
+git config --global user.name "CrazyFenX"
+git config --global user.email "kattoshkin@gmail.com"
+
+git remote add FactorioRepos https://github.com/CrazyFenX/FactorioRepos.git
+git remote show FactorioRepos
+
+@echo !!! Repos has been connected !!!
+
+@echo Check it. If it's good, press any key
+
+pause
+
+git add .
+git status
+git diff
+@echo Check your changes. If it's good, press any key
+
+pause
+
+rem Определяем текущую дату
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+set mytime=%time%
+
+rem Фиксируем изменения
+@echo Commiting...
+git commit -m "commit by %mydate%:%mytime%"
+@echo commit has been planted ^-^
+
+@echo Pushing...
+git push origin main
+@echo push has been planted 0.o
